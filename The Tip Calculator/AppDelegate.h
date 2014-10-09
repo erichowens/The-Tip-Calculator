b9290bc23adf9a1d850bcbbfe526f0af622c1bc4
Erich Owens <erichowens@fb.com>
2014-10-08 22:27:51 -0700

    Initial Commit

---
+++ The Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Tip Calculator
+//
+//  Created by Erich Owens on 10/8/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  The Tip Calculator
+//
+//  Created by Erich Owens on 10/8/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
